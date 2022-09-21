adcdac4e7803ca8316d962d701f5ff7fc78ad272
M.J.N.V.SAI <94554110+MJNVSai@users.noreply.github.com>
2022-09-21 11:11:00 +0530

    Add files via upload

---
+++ Single_Inheritance/Single_Inheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Single_Inheritance
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Programmer p = new Programmer();
+            Console.WriteLine("Employee Salary : " + p.salary);
+            Console.WriteLine("Employee Bonus : " + p.bonus);
+        }
+    }
+
+    class Employee
+    {
+        public float salary = 40000;
+    }
+
+    class Programmer : Employee
+    {
+        public float bonus = 10000;
+    }
+}
