83ed047fc43c923a9f888ba58f85f0ca04cdf833
M.J.N.V.SAI <94554110+MJNVSai@users.noreply.github.com>
2022-09-21 11:12:43 +0530

    Add files via upload

---
+++ Heirarchical_Inheritance/Heirarchical_Inheritance/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heirarchical_Inheritance
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            Cat c = new Cat();
+            d.Bark();
+            d.Hungar();
+
+            Console.WriteLine();
+
+            c.Hungar();
+            c.Sleep();
+        }
+    }
+
+    class Animal
+    {
+        public void Hungar()
+        {
+            Console.WriteLine(" I am Hungary ");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine(" Barking ");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public void Sleep()
+        {
+            Console.WriteLine(" Sleeping ");
+        }
+    }
+}
