cf105aa1ca33ba013e5cda0d2851b055c8976355
M.J.N.V.SAI <94554110+MJNVSai@users.noreply.github.com>
2022-09-21 11:15:15 +0530

    Add files via upload

---
+++ MultiLevel_Inheritance/MultiLevel_Inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiLevel_Inheritance
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BabyDog bd = new BabyDog();
+            bd.eat();
+            bd.weep();
+            bd.Bark();
+        }
+    }
+
+    class Animal
+    {
+        public void eat()
+        {
+            Console.WriteLine(" Eating ");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine(" Barking ");
+        }
+    }
+
+    class BabyDog : Dog
+    {
+        public void weep()
+        {
+            Console.WriteLine(" Weeping ");
+        }
+    }
+}
