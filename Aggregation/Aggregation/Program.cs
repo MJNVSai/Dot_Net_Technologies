1a8b37580e4fd4f87c8eb496c7dd134a9d48f554
M.J.N.V.SAI <94554110+MJNVSai@users.noreply.github.com>
2022-09-23 10:28:44 +0530

    Add files via upload

---
+++ Aggregation/Aggregation/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aggregation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Address a1 = new Address("Sabhapathi Street", "Vijayawada", "A.P");
+            Employee e = new Employee(156, "Rizwan", a1);
+            e.display();
+        }
+    }
+
+    class Address
+    {
+        public string addressLine, city, state;
+
+        public Address(string a, string c, string s)
+        {
+            this.addressLine = a;
+            this.city = c;
+            this.state = s;
+        }
+    }
+
+    class Employee
+    {
+        public int id;
+        public string name;
+        public Address address; // This is an aggregation : Has a Relationship
+
+        public Employee(int i, string s, Address a)
+        {
+            this.id = i;
+            this.name = s;
+            this.address = a;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Emplyee id : " + id + "\nEmployee Name : " + name);
+            Console.WriteLine("Address Line : " + address.addressLine + "\nCity : " + address.city + "\nState : " + address.state);
+        }
+    }
+
+}
---
+++ Static_Constructer/Static_Constructer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Static_Constructer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account(101, "Satya");
+            Account a2 = new Account(102, "Deva");
+
+            a1.Display();
+            a2.Display();
+        }
+    }
+
+    class Account
+    {
+        public int id;
+        public string name;
+        public static float RateInterest;
+
+        public Account(int id, string name)
+        {
+            Console.WriteLine(" Parameterized Constructor Invoked ");
+            this.id = id;
+            this.name = name;
+        }
+
+        static Account()
+        {
+            Console.WriteLine(" Static Constructor Invoked ");
+            RateInterest = 8.7f;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("\n\nEmployee ID : " + id + "\nEmployee Name : " + name + "\nRate of Interest : " + RateInterest);
+        }
+    }
+}
