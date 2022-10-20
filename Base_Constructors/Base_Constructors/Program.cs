3bbb64127d5021a91eb7125f91be9e2b2df43ab3
M.J.N.V.SAI <94554110+MJNVSai@users.noreply.github.com>
2022-10-20 19:26:20 +0530

    Add files via upload

---
+++ Base_Constructors/Base_Constructors/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_Constructors
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Python obj = new Python();
+            Python obj1 = new Python(50, "C++", 500);
+            obj.show();
+            Console.WriteLine();
+            obj1.show();
+        }
+    }
+
+    class Book
+    {
+        public int bno;
+        public string bname;
+        public float bcost;
+
+        public Book()
+        {
+            bno = 45;
+            bname = "OOPS With Java";
+            bcost = 250;
+        }
+
+        public Book(int bno, string bname, float bcost)
+        {
+            this.bno = bno;
+            this.bname = bname;
+            this.bcost = bcost;
+        }
+    }
+
+    class Python : Book
+    {
+        // Calling base class default and parameterized constructors through derived class
+
+        public Python() : base()
+        {
+            Console.WriteLine(" Python Constructor ");
+        }
+
+        public Python(int bno, string bname, float bcost) : base(bno, bname, bcost)
+        {
+            Console.WriteLine("Python Parameterized Constructor ");
+        }
+
+        public void show()
+        {
+            Console.WriteLine(" Book Number : " + bno);
+            Console.WriteLine(" Book name : " + bname);
+            Console.WriteLine(" Book Cost : " + bcost);
+        }
+    }
+}
---
+++ Base_Program/Base_Program/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_Program
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Cat c = new Cat();
+            c.Eat();
+            c.show();
+        }
+    }
+
+    class Animal
+    {
+        public string color = "White";
+        public Animal()
+        {
+            Console.WriteLine("This is a Animal Class Constructor");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public string color = "Black";
+        public void show()
+        {   // Calling the base class field attributes
+            Console.WriteLine("Parent Class Field Color : " + base.color); 
+            Console.WriteLine("Child Class Field Color : " + color);
+        }
+
+        public virtual void Eat()
+        {
+            Console.WriteLine(" I am Eating Food ");
+        }
+    }
+
+    class Cat : Dog
+    {
+        public override void Eat()
+        {
+            base.Eat(); // Calling the base class method
+            Console.WriteLine(" Drinking Milk ");
+        }
+    }
+}
